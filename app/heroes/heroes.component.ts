94641df056181bc44225be7ada24457f278c62b8
deskach <dzianis.kachanau@gmail.com>
2016-01-31 17:37:54 +1000

    Commented on how to deal with useClass useExisting & useValue

--- app/heroes/heroes.component.ts
+++ app/heroes/heroes.component.ts
@@ -9,7 +9,28 @@ import { Logger }               from './logger.service';
         <h2>Heroes</h2>
         <hero-list></hero-list>
   `,
+
+    /*
+    // A few examples
+     providers: [HeroService, provide(Logger, {useClass: BetterLogger})],
+
+     providers: [
+        HeroService,
+        NewLogger,
+        provide(Logger, {useClass: BetterLogger}),
+        provide(OldLogger, {useExisting: NewLogger})) // Alias OldLogger w/ reference to NewLogger
+     ],
+
+     // An object in the shape of the logger service
+     let silentLogger = {
+         logs: ['Silent logger says "Shhhhh!". Provided via "useValue"'],
+         log: () => {}
+     };
+     providers: [provide(Logger, {useValue: silentLogger})]
+
+   */
     providers: [HeroService, Logger],
+
     directives: [HeroListComponent]
 })
 
