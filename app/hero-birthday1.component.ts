08d0c83f18af98faf0cfb808a16c33f5ab399354
deskach <dzianis.kachanau@gmail.com>
2016-02-06 12:23:06 +1000

    Toggle format done

--- app/hero-birthday1.component.ts
+++ app/hero-birthday1.component.ts
@@ -2,8 +2,23 @@ import {Component} from 'angular2/core';
 
 @Component({
     selector: 'hero-birthday',
-    template: `<p>The hero's birthday is {{ birthday | date }}</p>`
+    template: `
+    <p>The hero's birthday is {{ birthday | date:format }}</p>
+    <button (click)="toggleFormat()">Toggle</button>
+    `
 })
 export class HeroBirthday {
-    birthday = new Date(1988,3,15); // April 15, 1988
+    public birthday = new Date(1988, 3, 15); // April 15, 1988
+
+    private formats = ['shortDate', 'fullDate', 'medium', 'short', 'longDate', 'mediumDate'];
+    private _index:number = 0;
+
+    public toggleFormat() {
+        this._index++;
+        this._index %= this.formats.length;
+    }
+
+    public get format() {
+        return this.formats[this._index];
+    }
 }
