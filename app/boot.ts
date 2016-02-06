d3d4446c3e0309b9446995a88efce8075018dc27
deskach <dzianis.kachanau@gmail.com>
2016-02-06 12:42:22 +1000

    Power Booster component added

--- app/boot.ts
+++ app/boot.ts
@@ -1,4 +1,6 @@
 import {bootstrap}    from 'angular2/platform/browser';
 import {HeroBirthday} from "./hero-birthday1.component";
+import {PowerBooster} from "./power-booster.component";
 
 bootstrap(HeroBirthday);
+bootstrap(PowerBooster);
