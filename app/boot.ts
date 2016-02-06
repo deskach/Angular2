a694f388497d57a5cb5edda8f16f9e77839a26d0
deskach <dzianis.kachanau@gmail.com>
2016-02-06 12:53:52 +1000

    Revert "Hero Birthday done"
    This reverts commit 084611cb14e4695fc803fce4ff91b1884119236d.

--- app/boot.ts
+++ app/boot.ts
@@ -1,4 +1,4 @@
-import {bootstrap}    from 'angular2/platform/browser';
-import {HeroBirthday} from "./hero-birthday1.component";
+import {bootstrap}    from 'angular2/platform/browser'
+import {AppComponent} from './app.component'
 
-bootstrap(HeroBirthday);
+bootstrap(AppComponent);
--- app/hero-birthday1.component.ts
+++
@@ -1,9 +0,0 @@
-import {Component} from 'angular2/core';
-
-@Component({
-    selector: 'hero-birthday',
-    template: `<p>The hero's birthday is {{ birthday | date }}</p>`
-})
-export class HeroBirthday {
-    birthday = new Date(1988,3,15); // April 15, 1988
-}
