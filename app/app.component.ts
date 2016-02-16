1d2ddc3ee75027db3c8a9d7785aca92fc44a9d96
deskach <dzianis.kachanau@gmail.com>
2016-02-17 06:28:52 +1000

    Re-arranged project structure

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,9 +1,12 @@
 import {Component} from 'angular2/core';
 import {InitCapsPipe} from "./init-caps/init-caps.pipe";
+import {Hero} from "./hero/hero";
 
 @Component({
     selector: 'my-app',
-    template: '<h1>{{"hello world!" | initCaps}}</h1>',
+    template: '<h1>{{"hello " + hero.name | initCaps}}</h1>',
     pipes: [InitCapsPipe]
 })
-export class AppComponent { }
+export class AppComponent {
+    public hero: Hero = new Hero(1, 'Bombasto');
+}
